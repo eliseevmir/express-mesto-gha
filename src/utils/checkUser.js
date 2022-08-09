bf3b2e1e01f301808087f2e340fd72f24c8370bb
Vasily Novosad <delhsmail@gmail.com>
2022-08-09 17:43:10 +0500

    Format errors

--- src/utils/checkUser.js
+++ src/utils/checkUser.js
@@ -15,32 +15,32 @@ const checkUserData = (user) => {
     if (field === "name") {
       if (typeof value !== "string") {
         resolve.check = false;
-        resolve.errors.push("Поле name не заполнено");
+        resolve.errors.push("Поле «name» не заполнено");
       }
       if (value.length > 30) {
         resolve.check = false;
-        resolve.errors.push("Длина поля name не должна превышать 30 символов");
+        resolve.errors.push("Длина поля «name» не должна превышать 30 символов");
       }
       if (value.length < 3) {
         resolve.check = false;
-        resolve.errors.push("Длина поля name не должна быть меньше 2 символов");
+        resolve.errors.push("Длина поля «name» не должна быть меньше 2 символов");
       }
     }
 
     if (field === "about") {
       if (typeof value !== "string") {
         resolve.check = false;
-        resolve.errors.push("Поле about не заполнено");
+        resolve.errors.push("Поле «about» не заполнено");
       }
 
       if (value.length > 30) {
         resolve.check = false;
-        resolve.errors.push("Длина поля about не должна превышать 30 символов");
+        resolve.errors.push("Длина поля «about» не должна превышать 30 символов");
       }
       if (value.length < 3) {
         resolve.check = false;
         resolve.errors.push(
-          "Длина поля about не должна быть меньше 2 символов"
+          "Длина поля «about» не должна быть меньше 2 символов"
         );
       }
     }
