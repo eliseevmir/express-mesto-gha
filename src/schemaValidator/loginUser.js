b3837b6f43be2615b5f896caaff89c8b5e63aa3e
VLADIMIR BURLYAEV <eliseevmir@gmail.com>
2022-08-23 17:35:53 +0500

    fix: fixed bugs

--- src/schemaValidator/loginUser.js
+++ src/schemaValidator/loginUser.js
@@ -1,8 +1,12 @@
 const { celebrate, Joi } = require("celebrate");
+const { EMAILREGEXP } = require("../utils/constants");
 
 module.exports = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().required().pattern(new RegExp(EMAILREGEXP)).messages({
+      "string.pattern.base": "Некорректный email",
+      "any.required": "Email обязателен",
+    }),
     password: Joi.string().required(),
   }),
 });
