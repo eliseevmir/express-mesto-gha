0799e12cf72f9a13e68c2cba61f29617cbf0aa8d
VLADIMIR BURLYAEV <eliseevmir@gmail.com>
2022-08-23 14:00:07 +0500

    delete utils/errors.js file

--- src/utils/errors.js
+++
@@ -1,8 +0,0 @@
-const errors = (props) => {
-  const { statusCode, error, res, message } = props;
-
-  console.log(error.prototype);
-  return res.status(statusCode).send({ error: true, message });
-};
-
-module.exports = errors;
